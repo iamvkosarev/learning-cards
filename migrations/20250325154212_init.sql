f2e07c113fd07460bb1e61c27d17467a1e611c73
Kosarev Vsevolod <iamvkosarev@gmail.com>
2025-03-26 20:11:40 +0800

    Fixed migration

--- migrations/20250325154212_init.sql
+++ migrations/20250325154212_init.sql
@@ -1,21 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS card_groups (
-                                           id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL,
-    name TEXT NOT NULL,
-    updated_at TIMESTAMPTZ DEFAULT now()
-    );
-CREATE TABLE IF NOT EXISTS cards (
-                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    group_id UUID NOT NULL REFERENCES card_groups(id) ON DELETE CASCADE,
-    front_text TEXT NOT NULL,
-    back_text TEXT NOT NULL
-    );
--- +goose StatementEnd
+	id BIGSERIAL PRIMARY KEY,
+	user_id BIGINT NOT NULL,
+	name TEXT NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT now()
+	);
 
+CREATE TABLE IF NOT EXISTS cards (
+	id BIGSERIAL PRIMARY KEY,
+	group_id BIGINT NOT NULL REFERENCES card_groups (id) ON DELETE CASCADE,
+	front_text TEXT NOT NULL,
+	back_text TEXT NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT now()
+	);
+
+-- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS cards;
+
 DROP TABLE IF EXISTS card_groups;
+
 -- +goose StatementEnd
